7a3057cc960e7a8d9f422d9f039cf551896b493e
Bruno Gomes Damaio <gomedamasiob@gmailcom>
2024-04-23 10:14:40 -0300

    app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+let contador = 1;
+
+while (contador < 4) 
+console.log(`Executando a iteração`+contador);
+
+contador = contador + 1;
+e o outro em seguida, apagando o primeiro.
+while (contador < 4) 
+alert(`Executando a iteração`+contador);
+
+contador = contador + 1;
